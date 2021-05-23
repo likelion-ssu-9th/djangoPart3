6dd8cc53020dc3e6192797995b4aec1173db838c
joseunghyo <seunghyo7742@naver.com>
2021-05-23 15:27:00 +0900

    examplePrj 완성

--- examplePrj/main/models.py
+++ examplePrj/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Blog(models.Model):
+    title=models.CharField(max_length=100)
+    pub_date=models.DateTimeField()
+    body=models.TextField()
+    author=models.CharField(max_length=100) #작성자를 직접 입력해줘야함.
+    
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:100]
 # Create your models here.
--- examplePrj/main/views.py
+++ examplePrj/main/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+from django.utils import timezone
+from .models import Blog
 
-# Create your views here.
+def home(request):
+    blogs= Blog.objects.all()
+    return render(request,"home.html",{"blogs":blogs})
+
+def new(request):
+    return render(request,'new.html')
+
+def create(request):
+    new_blog=Blog()
+    new_blog.title=request.POST['title']
+    new_blog.body=request.POST['body']
+    new_blog.pub_date= timezone.now()
+    new_blog.author=request.POST['author'] #바뀔부분
+    new_blog.save()
+    return redirect('home')
+
+def detail(request, blog_id) :
+    blog=get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html', {"blog":blog})
+
+def profile(request):
+    return render(request, 'profile.html')
+
