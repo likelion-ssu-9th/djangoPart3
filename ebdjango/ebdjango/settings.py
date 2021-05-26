9b7190a7c6818ee845f78302bcec3614cc895cfb
BaeJiYeong <jy111099@gmail.com>
2021-05-26 20:38:56 +0900

    Add host

--- ebdjango/ebdjango/settings.py
+++ ebdjango/ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5d^wqd&z02#5g@c5^p!#y_c_w(v_pflb%x35256+nn!u2gqp)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-env.eba-zbwipxjm.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
